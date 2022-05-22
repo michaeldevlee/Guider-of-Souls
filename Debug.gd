965f69f58ae98bb2af20bd84c23d56cfca7dee15
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2022-05-21 18:41:14 -0700

    Initial commit

---
+++ Debug.gd
@@ -0,0 +1,27 @@
+extends Node
+
+export var camera_path: NodePath 
+export var camera_speed_multiplier : int = 1;
+export var camera_speed : int =  20;
+
+func _input(event):
+	handle_camera_debug();
+
+func handle_camera_debug():
+	if camera_path == null:
+		return
+	
+	var camera = get_node(camera_path);
+	
+	if Input.is_action_pressed("ui_left"):
+		camera.position.x -= camera_speed * camera_speed_multiplier;
+	
+	if Input.is_action_pressed("ui_down"):
+		camera.position.y += camera_speed * camera_speed_multiplier;
+	
+	if Input.is_action_pressed("ui_right"):
+		camera.position.x += camera_speed * camera_speed_multiplier;
+	
+	if Input.is_action_pressed("ui_up"):
+		camera.position.y -= camera_speed * camera_speed_multiplier;
+		
