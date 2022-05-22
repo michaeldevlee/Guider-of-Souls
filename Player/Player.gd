10e5d11429f37aa0feb4a22fb34cbc347a671ec0
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2022-05-22 15:49:39 -0700

    collision corrections

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var anim_player = get_node("AnimationPlayer")
+
 export (int) var speed = 200
 var can_move : bool = true
 
@@ -8,14 +10,10 @@ var velocity = Vector2()
 func _ready():
 	initialize_signals()
 
-func initialize_signals():
-	EventBus.connect("update_player_move_status", self, "set_move_status")
-
-func set_move_status(status : bool):
-	can_move = status
-	velocity = Vector2()
-
 func get_input():
+	
+	if velocity == Vector2():
+		anim_player.play("Idle")
 	
 	if !can_move:
 		return
@@ -23,13 +21,24 @@ func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		anim_player.play("Walk Right")
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		anim_player.play("Walk Left")
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		anim_player.play("Walk Down")
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		anim_player.play("Walk Up")
 	velocity = velocity.normalized() * speed
+
+func initialize_signals():
+	EventBus.connect("update_player_move_status", self, "set_move_status")
+
+func set_move_status(status : bool):
+	can_move = status
+	velocity = Vector2()
 
 func _physics_process(delta):
 	get_input()
